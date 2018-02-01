feef0070c970cee27f212972685fcbcdb719b815
Norbert Reschke <reschke@hsbkob.de>
2018-02-01 17:33:48 +0100

    geändert:       cover.rb

--- cover.rb
+++ cover.rb
@@ -11,4 +11,4 @@ rectangle(   0,   0, 200,1000,:stroke=>"none",:fill=>"#C7C7C7") # background
      text( 180, 800,"font-family"=>"sans","font-size"=>100,:fill=>"white"){raw "Kompetenz"}.rotate(-90,180,800) # label
 end
 
-File.open("site/images/cover.svg", "w") { |f| img.write(f) } # store svg on disk
+File.open("a241/images/cover.svg", "w") { |f| img.write(f) } # store svg on disk
