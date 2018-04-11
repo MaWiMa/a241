251387717466da71d61c777ad071535a82768fb5
Norbert Reschke <reschke@hsbkob.de>
2018-04-11 14:08:40 +0200

    geändert:       cover.rb

--- cover.rb
+++ cover.rb
@@ -1,14 +1,18 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 # encoding: utf-8
-
+require_relative 'whatDir'
 require 'rasem'
 # https://github.com/aseldawy/rasem
 
+file = A241DIR.tempdirs[2] + "/cover.svg"
 
-img = Rasem::SVGImage.new(:width => 200, :height => 1000) do
+img = Rasem::SVGImage.new(:width => 200, :height => 800) {
+  rectangle(0,0,200,800,:stroke=>"none",:fill=>"#C7C7C7") # background 
+  text(180,600,"font-family"=>"sans","font-size"=>100,:fill=>"white"){raw "Kompetenz"}.rotate(-90,180,600) # label
+}
 
-rectangle(   0,   0, 200,1000,:stroke=>"none",:fill=>"#C7C7C7") # background 
-     text( 180, 800,"font-family"=>"sans","font-size"=>100,:fill=>"white"){raw "Kompetenz"}.rotate(-90,180,800) # label
+if !File.exists?(file)
+  File.open(file, "w") { |f|
+    img.write(f)
+  }
 end
-
-File.open("a241/images/cover.svg", "w") { |f| img.write(f) } # store svg on disk
