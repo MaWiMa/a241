5005cdcfbdf54e8cb788b4c140ae33eeaefc7b0f
Norbert Reschke <reschke@hsbkob.de>
2018-04-11 14:08:54 +0200

    geändert:       links.rb

--- links.rb
+++ links.rb
@@ -1,41 +1,52 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 # encoding: utf-8
-
+require_relative 'whatDir'
 require 'rasem'
 # https://github.com/aseldawy/rasem
 
 # go back to lastpage
-img = Rasem::SVGImage.new(:width => 500, :height => 50) do
+file = A241DIR.tempdirs[2] + "/back.svg"
+img = Rasem::SVGImage.new(:width => 500, :height => 50) {
+  rectangle(0,0,500,50,:stroke=>"none",:fill=>"none") # background 
+  text(200,40,"font-family"=>"sans","font-size"=>40,:fill=>"white"){raw "back"}
+  line(30,25,170,25,:stroke=>"white",:stroke_width=>10)
+  line(330,25,490,25,:stroke=>"white",:stroke_width=>10)
+  polygon([[0,25],[30,15],[30,35]],:stroke=>"none",:fill=>"white") # arrow
+}
 
-rectangle(0,0,500,50,:stroke=>"none",:fill=>"none") # background 
-     text(200,40,"font-family"=>"sans","font-size"=>40,:fill=>"white"){raw "back"}
-     line(30,25,170,25,:stroke=>"white",:stroke_width=>10)
-     line(330,25,490,25,:stroke=>"white",:stroke_width=>10)
-  
-polygon([[0,25],[30,15],[30,35]],:stroke=>"none",:fill=>"white") # arrow
+if !File.exists?(file)
+  File.open(file, "w") { |f|
+    img.write(f)
+  }
 end
 
-File.open("a241/images/back.svg", "w") { |f| img.write(f) } # store svg on disk
+# go to homepage
+file = A241DIR.tempdirs[2] + "/home.svg"
+img = Rasem::SVGImage.new(:width => 500, :height => 50) {
+  rectangle(0,0,500,50,:stroke=>"none",:fill=>"none") # background 
+  text(200,40,"font-family"=>"sans","font-size"=>40,:fill=>"white"){raw "home"}
+  line(10,25,170,25,:stroke=>"white",:stroke_width=>10)
+  line(330,25,490,25,:stroke=>"white",:stroke_width=>10)
+}
 
-# home page 
-img = Rasem::SVGImage.new(:width => 500, :height => 50) do
-
-rectangle(0,0,500,50,:stroke=>"none",:fill=>"none") # background 
-     text(200,40,"font-family"=>"sans","font-size"=>40,:fill=>"white"){raw "home"}
-     line(10,25,170,25,:stroke=>"white",:stroke_width=>10)
-     line(330,25,490,25,:stroke=>"white",:stroke_width=>10)
- end
-
-File.open("a241/images/home.svg", "w") { |f| img.write(f) } # store svg on disk
-# next page 
-img = Rasem::SVGImage.new(:width => 500, :height => 50) do
-
-rectangle(0,0,500,50,:stroke=>"none",:fill=>"none") # background 
-     text(200,40,"font-family"=>"sans","font-size"=>40,:fill=>"white"){raw "next"}
-     line(10,25,170,25,:stroke=>"white",:stroke_width=>10)
-     line(330,25,470,25,:stroke=>"white",:stroke_width=>10)
-  
-polygon([[500,25],[470,15],[470,35]],:stroke=>"none",:fill=>"white") # arrow
+if !File.exists?(file)
+  File.open(file, "w") { |f|
+    img.write(f)
+  }
 end
 
-File.open("a241/images/next.svg", "w") { |f| img.write(f) } # store svg on disk
+# go to next page
+file = A241DIR.tempdirs[2] + "/next.svg"
+img = Rasem::SVGImage.new(:width => 500, :height => 50) {
+  rectangle(0,0,500,50,:stroke=>"none",:fill=>"none") # background 
+  text(200,40,"font-family"=>"sans","font-size"=>40,:fill=>"white"){raw "next"}
+  line(10,25,170,25,:stroke=>"white",:stroke_width=>10)
+  line(330,25,470,25,:stroke=>"white",:stroke_width=>10)
+  polygon([[500,25],[470,15],[470,35]],:stroke=>"none",:fill=>"white") # arrow
+}
+
+if !File.exists?(file)
+  File.open(file, "w") { |f|
+    img.write(f)
+  }
+end
